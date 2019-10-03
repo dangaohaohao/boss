cf94900f37a224dde1ac852df27fd5ceb87cdc94
yuzhenliu <1109763686@qq.com>
2019-10-03 23:29:11 +0800

    写了 user 我的页面

---
+++ static/js/user.js
@@ -0,0 +1,15 @@
+ 
+ // 初始化   求职助手中左右的
+
+  let $utilsWrap = $('.utilsWrap');
+  let myutilScroll = new IScroll($utilsWrap[0], {
+    click: true,
+    tap: true,
+    scrollX: true,
+    scrollY: false,
+    startX: 0,
+    startY: 0
+  });
+
+
+
